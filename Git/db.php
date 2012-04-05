bd40c67461ed0f190c3c9bae5add143f3a494db6
Aditya Parab <adiparab14@gmail.com>
2012-04-05 07:54:29 +0530

    Updated

---
+++ Git/db.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost'; //usually localhost
+$username = 'root'; //your username assigned to your database
+$password = 'madhukar'; //your password assigned to your user & database
+$database = 'phpmyadmin'; //your database name
+?>
